6f00b363714cc477081cf8cdcd9f257bb566b4d0
tonxiaol <134091784+tonxiaol@users.noreply.github.com>
2023-06-23 13:37:54 -0400

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -15,7 +15,7 @@ package midtermexam_versiona_extensioncode;
  */
 public class UserProfile 
 {
-    
+    // this is a comment
     private String userID;//the userID
     private String genre;// the user's preferred genre of movie
     private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
